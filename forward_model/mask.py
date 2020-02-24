6cdd1a83a8a23a960b44fc1fa85feb3e12c977e7
chihchunhsu <chh194@ucsd.edu>
2020-02-23 20:36:36 -0800

    change to KDE move

---
+++ forward_model/mask.py
@@ -0,0 +1,9 @@
+import smart
+import numpy as np
+import matplotlib.pyplot as plt
+
+#--------------------------------------------
+# 20200223 chsu: routine to generate the mask 
+#--------------------------------------------
+
+class 
