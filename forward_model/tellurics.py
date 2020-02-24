11b49f081a7e72fad685f416062f0ef7e2c6eb4c
ctheissen <ctheissen@gmail.com>
2020-02-24 13:44:24 -0800

    Fixes for telluric interpolation

--- forward_model/tellurics.py
+++ forward_model/tellurics.py
@@ -91,78 +91,6 @@ def InterpTelluricModel(wavelow, wavehigh, airmass, pwv):
     FULL_PATH  = os.path.realpath(__file__)
     BASE, NAME = os.path.split(FULL_PATH)
 
-    def bilinear_interpolation(x, y, points):
-        '''Interpolate (x,y) from values associated with four points.
-
-        The four points are a list of four triplets:  (x, y, value).
-        The four points can be in any order.  They should form a rectangle.
-
-            >>> bilinear_interpolation(12, 5.5,
-            ...                        [(10, 4, 100),
-            ...                         (20, 4, 200),
-            ...                         (10, 6, 150),
-            ...                         (20, 6, 300)])
-            165.0
-
-        '''
-        # See formula at:  http://en.wikipedia.org/wiki/Bilinear_interpolation
-
-        #print('X', x)
-        #print('Y', y)
-        #print('Points', points)
-
-        try:
-            points = sorted(points)               # order points by x, then by y
-            (x1, y1, q11), (_x1, y2, q12), (x2, _y1, q21), (_x2, _y2, q22) = points
-
-            if x1 != _x1 or x2 != _x2 or y1 != _y1 or y2 != _y2:
-                raise ValueError('points do not form a rectangle')
-            if not x1 <= x <= x2 or not y1 <= y <= y2:
-                raise ValueError('(x, y) not within the rectangle')
-    
-            return 10**( (q11 * (x2 - x) * (y2 - y) +
-                          q21 * (x - x1) * (y2 - y) +
-                          q12 * (x2 - x) * (y - y1) +
-                          q22 * (x - x1) * (y - y1)
-                       ) / ( (x2 - x1) * (y2 - y1) + 0.0) )
-
-        except:
-            # Linear interpolation along one axis
-            # handling linear interpolation, it does not matter which y1 or y2 is larger
-            # see formula at: https://en.wikipedia.org/wiki/Linear_interpolation
-            (x1, y1, q11), (_x1, y2, q12), (x2, _y1, q21), (_x2, _y2, q22) = points
-
-
-            if x2 == x1 == _x1 == _x2: # Need to linearly interpolate along the y axis
-                return 10**( ( q11 * ( y2 - y ) + q22 * ( y - y1 ) ) / ( ( y2 - y1 ) ) )
-
-            if y2 == y1 == _y1 == _y2: # Need to linearly interpolate along the x axis
-                return 10**( ( q11 * ( x2 - x ) + q22 * ( x - x1 ) ) / ( ( x2 - x1 ) ) )
-
-            if y1 != _y1 or y2 != _y2: 
-                if y1 == y2 and _y1 == _y2:
-                    y2  = _y1
-                    q12 = q21
-
-                    return 10**( ( q11 * ( y2 - y ) + q12 * ( y - y1 ) ) / ( ( y2 - y1 ) ) )
-
-                else:
-                    raise ValueError('points do not form a line') 
-
-            if x1 != _x1 or x2 != _x2:
-                if x1 == x2 and _x1 == _x2:
-                    x2  = _x1
-                    q11 = q22
-
-                    return 10**( ( q21 * ( x2 - x ) + q22 * ( x - x1 ) ) / ( ( x2 - x1 ) ) )
-
-                else:
-                    raise ValueError('points do not form a line') 
-
-
-    def myround(x, base=.5):
-        return base * round(float(x)/base)
-
     Gridfile = BASE + '/../libraries/telluric/pwv_R300k_gridparams.csv'
 
     T1 = Table.read(Gridfile)
@@ -187,7 +115,8 @@ def InterpTelluricModel(wavelow, wavehigh, airmass, pwv):
                 return 1
     
     # Get the four points
-    Points =  [ [T1['airmass'][np.where( (T1['airmass'] == x1) & (T1['pwv'] == y1))], T1['pwv'][np.where((T1['airmass'] == x1) & (T1['pwv'] == y1))], np.log10(GetModel(wavelow=wavelow, wavehigh=wavehigh, airmass=T1['airmass'][np.where( (T1['airmass'] == x1) & (T1['pwv'] == y1))][0], pwv=T1['pwv'][np.where((T1['airmass'] == x1) & (T1['pwv'] == y1))][0]))],
+    Points =  [ 
+                [T1['airmass'][np.where( (T1['airmass'] == x1) & (T1['pwv'] == y1))], T1['pwv'][np.where((T1['airmass'] == x1) & (T1['pwv'] == y1))], np.log10(GetModel(wavelow=wavelow, wavehigh=wavehigh, airmass=T1['airmass'][np.where( (T1['airmass'] == x1) & (T1['pwv'] == y1))][0], pwv=T1['pwv'][np.where((T1['airmass'] == x1) & (T1['pwv'] == y1))][0]))],
                 [T1['airmass'][np.where( (T1['airmass'] == x1) & (T1['pwv'] == y2))], T1['pwv'][np.where((T1['airmass'] == x1) & (T1['pwv'] == y2))], np.log10(GetModel(wavelow=wavelow, wavehigh=wavehigh, airmass=T1['airmass'][np.where( (T1['airmass'] == x1) & (T1['pwv'] == y2))][0], pwv=T1['pwv'][np.where((T1['airmass'] == x1) & (T1['pwv'] == y2))][0]))],
                 [T1['airmass'][np.where( (T1['airmass'] == x2) & (T1['pwv'] == y1))], T1['pwv'][np.where((T1['airmass'] == x2) & (T1['pwv'] == y1))], np.log10(GetModel(wavelow=wavelow, wavehigh=wavehigh, airmass=T1['airmass'][np.where( (T1['airmass'] == x2) & (T1['pwv'] == y1))][0], pwv=T1['pwv'][np.where((T1['airmass'] == x2) & (T1['pwv'] == y1))][0]))],
                 [T1['airmass'][np.where( (T1['airmass'] == x2) & (T1['pwv'] == y2))], T1['pwv'][np.where((T1['airmass'] == x2) & (T1['pwv'] == y2))], np.log10(GetModel(wavelow=wavelow, wavehigh=wavehigh, airmass=T1['airmass'][np.where( (T1['airmass'] == x2) & (T1['pwv'] == y2))][0], pwv=T1['pwv'][np.where((T1['airmass'] == x2) & (T1['pwv'] == y2))][0]))],
@@ -195,7 +124,7 @@ def InterpTelluricModel(wavelow, wavehigh, airmass, pwv):
 
     waves2 = GetModel(wavelow=wavelow, wavehigh=wavehigh, airmass=T1['airmass'][np.where( (T1['airmass'] == x1) & (T1['pwv'] == y1))][0], pwv=T1['pwv'][np.where((T1['airmass'] == x1) & (T1['pwv'] == y1))][0], wave=True)
 
-    return waves2, bilinear_interpolation(airmass, pwv, Points)
+    return waves2, smart.utils.interpolations.bilinear_interpolation(airmass, pwv, Points)
 
 def convolveTelluric(lsf, airmass, pwv, telluric_data):
     """
