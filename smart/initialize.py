eaf9c003a1863c69e770e1f31c9284e665800ffd
ctheissen <ctheissen@gmail.com>
2024-07-26 12:42:29 -0700

    New 5-d interpolation method

--- smart/initialize.py
+++ smart/initialize.py
@@ -9,14 +9,23 @@ from astropy.table import Table
 ModelSets = {
     'phoenix-aces-agss-cond-2011' : 'PHOENIX_ACES_AGSS_COND_2011',
     'phoenix-btsettl-cifist2011' : 'PHOENIX_BTSETTL_CIFIST2011',
-    'phoenix_btsettl_cifist2011_2015' : 'PHOENIX_BTSETTL_CIFIST2011_2015',
-    'sonora' : 'SONORA_2018',
+    'phoenix-btsettl-cifist2011-2015' : 'PHOENIX_BTSETTL_CIFIST2011_2015',
     'phoenix-bt-dusty' : 'PHOENIX_BT_DUSTY',
     'drift-phoenix' : 'DRIFT_PHOENIX',
     'marcs-apogee-dr15' : 'MARCS_APOGEE_DR15',
     'marcs-pp' : 'MARCS_PP',
     'btsettl08' : 'BTSETTL08',
-    'phoenix-btsettl08' : 'PHOENIX_BTSETTL08'
+    'phoenix-btsettl08' : 'PHOENIX_BTSETTL08',
+    'atmo-2020-ceq' : 'ATMO_2020_CEQ',
+    'atmo-2020-neq-strong' : 'ATMO_2020_NEQ_STRONG',
+    'atmo-2020-neq-weak' : 'ATMO_2020_NEQ_WEAK',
+    'barman-20230830' : 'BARMAN_20230830',
+    'pheonix-newera-aces-cond-2023' : 'PHOENIX_NEWERA_ACES_COND_2023',
+    'hd206893-qkbbhires': 'HD206893_QKBBHIRES',
+    'sonora-2018' : 'SONORA_2018', # bobcat
+    'sonora-2021' : 'SONORA_2021', # cholla
+    'sonora-2023' : 'SONORA_2023', # elfowl
+    'sonora-2024' : 'SONORA_2024'  # diamondback
 }
 
 def getModelgrid(modelset = 'phoenix-aces-agss-cond-2011'):
@@ -25,7 +34,8 @@ def getModelgrid(modelset = 'phoenix-aces-agss-cond-2011'):
     # Get the gridfile for the requested modelset    
     FULL_PATH  = os.path.realpath(__file__)
     BASE, NAME = os.path.split(FULL_PATH)
-    Gridfile = BASE + '/libraries/%s/%s_gridparams.csv'%(smart.ModelSets[modelset.upper()], smart.ModelSets[modelset.upper()])
+    Gridfile = BASE + '/libraries/%s/%s_gridparams.csv'%(smart.ModelSets[modelset.lower()], smart.ModelSets[modelset.lower()])
+    print('Gridfile:', Gridfile)
     # Read the grid file
     T1 = Table.read(Gridfile)
 
