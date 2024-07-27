bbdc25b4548de8342f2848e529e7eb773f7dc0a4
ctheissen <ctheissen@gmail.com>
2024-07-26 18:29:47 -0700

    small fix

--- smart/forward_model/classModel.py
+++ smart/forward_model/classModel.py
@@ -135,22 +135,22 @@ class Model():
             self.flux = flux # erg/s/cm^2/Angstrom
 
         else:
-            #try:
-            self.teff     = kwargs.get('teff', 2500)
-            self.logg     = kwargs.get('logg', 5.00)
-            self.metal    = kwargs.get('metal', 0.00)
-            self.en       = kwargs.get('en', 0.00)
-            self.kzz      = kwargs.get('kzz', 0.00)
-            self.co       = kwargs.get('co', 0.00)
-            self.modelset = kwargs.get('modelset', 'btsettl08')
+            try:
+                self.teff     = kwargs.get('teff', 2500)
+                self.logg     = kwargs.get('logg', 5.00)
+                self.metal    = kwargs.get('metal', 0.00)
+                self.en       = kwargs.get('en', 0.00)
+                self.kzz      = kwargs.get('kzz', 0.00)
+                self.co       = kwargs.get('co', 0.00)
+                self.modelset = kwargs.get('modelset', 'btsettl08')
 
-            wave, flux = smart.forward_model.InterpolateModel.InterpModel(self.teff, self.logg, self.metal, self.en, self.kzz, self.co,
-                                                                          modelset=self.modelset, order=self.order, instrument=self.instrument)
-            self.wave = wave # Angstrom
-            self.flux = flux # erg/s/cm^2/Angstrom
-            #except:
-            #    self.wave   = kwargs.get('wave', [])
-            #    self.flux   = kwargs.get('flux', [])
+                wave, flux = smart.forward_model.InterpolateModel.InterpModel(self.teff, self.logg, self.metal, self.en, self.kzz, self.co,
+                                                                              modelset=self.modelset, order=self.order, instrument=self.instrument)
+                self.wave = wave # Angstrom
+                self.flux = flux # erg/s/cm^2/Angstrom
+            except:
+                self.wave   = kwargs.get('wave', [])
+                self.flux   = kwargs.get('flux', [])
         
 
     def plot(self, **kwargs):
